ec2824de92e5f2a1940d972d5a4d7627f8ca8bfe
Newaz Nezif <newaznezif@gmail.com>
2026-04-28 08:16:51 +0300

    Fix final typescript errors blocking Netlify

--- src/components/three/Background.tsx
+++ src/components/three/Background.tsx
@@ -3,6 +3,7 @@
 import { useRef, useMemo } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial } from "@react-three/drei";
+// @ts-ignore
 import * as random from "maath/random/dist/maath-random.esm";
 
 function ParticleField(props: any) {
